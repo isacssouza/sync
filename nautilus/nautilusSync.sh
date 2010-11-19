3f6e36f6f7156d806307da32c6ddac9440607163
Isac Sacchi e Souza <isacssouza@gmail.com>
2010-11-18 22:52:09 -0200

    Improving nautilus script.
    syncing stops when the progress window is closed.
    check if the user pressed Ok and typed some text before starting the
    sync.

--- nautilus/nautilusSync.sh
+++ nautilus/nautilusSync.sh
@@ -1,2 +1,14 @@
-dest=`zenity --entry --text='Enter the destination path'`
-sync.sh $1 $dest | zenity --progress --pulsate --auto-kill --no-cancel --title="Synchronizing..." --text="$1\n-->\n$dest"
+#!/bin/bash
+
+# script to be used with nautilus-actions.
+# $1 is the source/dir to be synced.
+
+# ask user for the destination path.
+dest=`zenity --entry --text='Enter the destination path. e.g. user@host:/path/to/dir/'`
+
+# if not empty
+if [ $dest ]
+then
+    #do the sync and pipe into a progress window. When the progess window dies, the sync dies too.
+    sync.sh $1 $dest | zenity --progress --pulsate --auto-kill --title="Synchronizing..." --text="$1\n-->\n$dest\n\nPress Cancel to stop syncing."
+fi
